180283081309f5fe186f89c5934993952a423798
Vignesh Chandrasekharan <vchan444@gmail.com>
2020-03-15 01:31:52 -0500

    created new basic structure for frontend

---
+++ nfront/src/components/resortInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Just an example component. We will create components like these and render them in App.js.
+
+class ResortInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is our resort info yo</h1>
+            </div>
+        )
+    }
+}
+
+export default ResortInfo;
