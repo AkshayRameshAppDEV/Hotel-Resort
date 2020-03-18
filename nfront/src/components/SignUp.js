0944712ee0bb5ff079c5d6d8ca9d0b0eb03e2fc3
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2020-03-17 20:10:33 -0500

    Added Sign Up UI and links

--- nfront/src/components/SignUp.js
+++ nfront/src/components/SignUp.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import '../styles/SignUp.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 // Just an example component. We will create components like these and render them in App.js.
 
 class SignUp extends React.Component {
     render() {
         return (
-            <div>
-                <h1>This is the sign up page</h1>
+
+            <div className="center-screen signUpDivBody">
+                <div className="auth-wrapper">
+                    <div className="auth-inner">
+
+                        <form action= {<Link to="/"></Link>}>
+                            <h3>Sign Up</h3>
+
+                            <div className="form-group">
+                                <label>First name</label>
+                                <input type="text" className="form-control" placeholder="First name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Last name</label>
+                                <input type="text" className="form-control" placeholder="Last name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email address</label>
+                                <input type="email" className="form-control" placeholder="Enter email" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+
+                            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                            <p className="forgot-password text-right">
+                                Already registered {<Link to="/login">sign in?</Link>}
+                            </p>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
