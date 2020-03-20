cab375de965d2b485b947ad36d989ab5f227138b
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2020-03-20 01:11:25 -0500

    Added redirect to rooms list page when checkAvailability button is pressed from Stick navbar

--- Frontend/src/components/CheckAvailabilityNavbar.js
+++ Frontend/src/components/CheckAvailabilityNavbar.js
@@ -2,17 +2,36 @@ import React from 'react';
 import '../styles/CheckAvailabilityNavbar.css';
 
 import {
-    Link
+    Link, Redirect
 } from "react-router-dom";
 
 // This is the bootstrap navbar component which is used in App.js
 
 class CheckAvailabilityNavbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: false,
+        };
+
+        this.submitPressed = this.submitPressed.bind(this);
+    }
+
+    submitPressed = event => {
+
+        this.setState({ redirect: true })
+
+    }
+
     render() {
+        if (this.state.redirect) {
+
+            return (<Redirect to="/roomlist" />);
+        }
         return (
             <nav class="navbar sticky-top bg-warning">
                 <div class="navbar-brand">Booking Your Hotel</div>
-                <form class="form-inline">
+                <form class="form-inline" onSubmit={this.submitPressed}>
                     <label for="checkIn">Check In:</label>
                     <input class="form-control" type="date" id="checkIn" name="checkIn" />
                     <label for="checkOut">Check Out:</label>
