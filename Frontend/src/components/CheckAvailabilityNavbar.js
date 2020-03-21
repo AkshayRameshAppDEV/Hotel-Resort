db08da52a253279852bff312864f32137c186d8b
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2020-03-20 21:55:16 -0500

    Added checkIn and CheckOut date input validation
    CheckIn date should be shown from current system date onwards
    
    Checkout date should be shown from checkin date onwards

--- Frontend/src/components/CheckAvailabilityNavbar.js
+++ Frontend/src/components/CheckAvailabilityNavbar.js
@@ -15,11 +15,44 @@ class CheckAvailabilityNavbar extends React.Component {
         };
 
         this.submitPressed = this.submitPressed.bind(this);
+        this.handleCheckInDateChange = this.handleCheckInDateChange.bind(this);
+        this.getCurrentDateInYYYYMMDDFormat = this.getCurrentDateInYYYYMMDDFormat.bind(this);
     }
+
+    // as the user changes the check in date update the checkout date to start from check in date onwards
+    handleCheckInDateChange = event => {
+        this.refs.checkOutDateRef.setAttribute('min', event.target.value);
+    };
 
     submitPressed = event => {
 
         this.setState({ redirect: true })
+
+    }
+
+    getCurrentDateInYYYYMMDDFormat = () => {
+
+        var d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
+    componentDidMount() {
+
+
+        //DISPLAYS FROM CURRENT DATE ONWARDS BY DEFAULT AND PREVIOUS DATE ARE DISABLED
+        this.refs.checkInDateRef.setAttribute('min', this.getCurrentDateInYYYYMMDDFormat());
+        this.refs.checkOutDateRef.setAttribute('min', this.getCurrentDateInYYYYMMDDFormat());
+
+
 
     }
 
@@ -33,9 +66,9 @@ class CheckAvailabilityNavbar extends React.Component {
                 <div class="navbar-brand">Booking Your Hotel</div>
                 <form class="form-inline" onSubmit={this.submitPressed}>
                     <label for="checkIn">Check In:</label>
-                    <input class="form-control" type="date" id="checkIn" name="checkIn" />
+                    <input class="form-control" type="date" id="checkIn" name="checkIn" ref="checkInDateRef" onChange={this.handleCheckInDateChange} required/>
                     <label for="checkOut">Check Out:</label>
-                    <input class="form-control" type="date" id="checkOut" name="checkOut" />
+                    <input class="form-control" type="date" id="checkOut" name="checkOut" ref="checkOutDateRef" required/>
 
                     <label for="guests">Guests:</label>
                     <select class="form-control" id="guests">
@@ -49,8 +82,6 @@ class CheckAvailabilityNavbar extends React.Component {
                     <label for="rooms">Room:</label>
                     <select class="form-control" id="rooms">
                         <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
                     </select>
 
                     <input class="submitButtonColor" type="submit" value="CHECK AVAILABILITY" />
