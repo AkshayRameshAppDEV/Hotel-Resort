7ae56b5259badce73040043129ced801f54155ea
Vignesh Chandrasekharan <vchan444@gmail.com>
2020-05-02 14:03:53 -0500

    added userprofile component to routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,6 +14,7 @@ import SignUp from './components/SignUp';
 import RoomList from './components/RoomList';
 import Payments from './components/Payments';
 import OrderConfirmation from './components/OrderConfirmation';
+import UserProfile from './components/UserProfile';
 
 // Entry point of the app. This will now act as the router component. 
 // Home.js is the homepage component.
@@ -46,6 +47,10 @@ function App() {
 
           <Route path="/orderconfirmation">
             <OrderConfirmation />
+          </Route>
+
+          <Route path="/userprofile">
+            <UserProfile />
           </Route>
 
           <Route path="/">
